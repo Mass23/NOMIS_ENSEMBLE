caf6788f3a743a00c0290270b33b555bf3aadeaf
Susheel Busi <susheel.busi@uni.lu>
2021-02-16 15:40:29 +0100

    added rules for tRNAscanning, mafft, consensus sequence and diamond analyses of KEGG clusters

--- metaG/get_clusterID.sh
+++ metaG/get_clusterID.sh
@@ -14,7 +14,7 @@ gp_edited.apply(lambda x: x.to_csv(str(x.name) + '.txt', sep='\t', header=False,
 # saving old cluster files before creating new ones
 # mkdir orig_cluster_files && mv *.txt orig_cluster_files
 
-df=pd.read_csv("clusters_min_7_samples.tsv", sep='\t')
+df=pd.read_csv("Unassigned_only_min_7_samples.tsv", sep='\t')
 df.drop(df.columns[0], axis=1, inplace=True)
 df.drop(df.columns[1], axis=1, inplace=True)
 new_df = df[~df.Sequence.str.contains("K", na=False)]
@@ -23,3 +23,15 @@ gp = new_df.groupby('ClusterID')
 gp_edited = gp[['Sequence']]
 gp_edited.apply(lambda x: x.to_csv(str(x.name) + '.txt', sep='\t', header=False, index=False)) # output multiple files based on common values in column (https://stackoverflow.com/questions/37216230/output-multiple-files-based-on-column-value-python-pandas)
 
+# for those with min-9 sequnces and at least in 2 samples
+df=pd.read_csv("clusters_min_9_seq_2_samp.tsv", sep='\t')
+df.drop(df.columns[0], axis=1, inplace=True)
+gp=df.groupby('ClusterID')
+gp_edited=gp[['Sequence']]
+gp_edited.apply(lambda x: x.to_csv(str(x.name) + '.txt', sep='\t', header=False, index=False))
+
+# Did the following in BASH to get the file clusters. Commented here due to python script
+# for the min_7 and clusters_min_9_seq_2 samples, have to rename all the files to have an underscore (_)
+# renaming part of filename
+# rename "Cluster" "Cluster_" *.txt
+# ls -1 *.txt | sed 's/Cluster_//g' | sed 's/.txt//g' > cluster_list
